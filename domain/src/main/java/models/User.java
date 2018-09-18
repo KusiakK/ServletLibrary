7ec3c054846cdd5ef28b5431d1c95cdb9a69316d
blysku <blyskx@gmail.com>
2018-09-18 19:29:25 +0200

    user minor changes 2

--- domain/src/main/java/models/User.java
+++ domain/src/main/java/models/User.java
@@ -10,13 +10,28 @@ public class User {
 
     @Id
     @Column(name = "user_id")
+    private Integer userID;
+
+    @Column(name = "user_name")
     private String userName;
+
+    @Column(name = "password")
+    private String password;
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
 
     @OneToMany(mappedBy = "userID")
     private Set<Borrow> borrows;
@@ -60,29 +75,89 @@ public class User {
         this.userDetails = userDetails;
     }
 
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Borrow> getBorrows() {
+        return borrows;
+    }
+
+    public void setBorrows(Set<Borrow> borrows) {
+        this.borrows = borrows;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(userName, user.userName) &&
+        return Objects.equals(userID, user.userID) &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(password, user.password) &&
                 Objects.equals(firstName, user.firstName) &&
                 Objects.equals(lastName, user.lastName) &&
+                Objects.equals(phoneNumber, user.phoneNumber) &&
+                Objects.equals(address, user.address) &&
+                Objects.equals(city, user.city) &&
+                Objects.equals(borrows, user.borrows) &&
                 Objects.equals(userDetails, user.userDetails);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userName, firstName, lastName, userDetails);
+        return Objects.hash(userID, userName, password, firstName, lastName, phoneNumber, address, city, borrows, userDetails);
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "borrowerID=" + userName +
+                "userID=" + userID +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", borrows=" + borrows +
                 ", userDetails=" + userDetails +
                 '}';
     }
