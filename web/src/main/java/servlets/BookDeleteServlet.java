17faf4da98fc1cc4f876d5fcf84c030fa3b2a29d
blysku <blyskx@gmail.com>
2018-09-18 20:10:27 +0200

    working on show books details page

--- web/src/main/java/servlets/BookDeleteServlet.java
+++ web/src/main/java/servlets/BookDeleteServlet.java
@@ -18,31 +18,36 @@ public class BookDeleteServlet extends HttpServlet {
 
         if (!"confirm".equals(req.getParameter("deleteConfirmation"))) {
             resp.sendRedirect("browse");
-        }
-        //TODO fix delete cancel button
-        Integer bookID = null;
-
-        try {
-            bookID = Integer.parseInt(req.getParameter("book-id"));
-        } catch (NumberFormatException e) {
+            return;
         }
 
-        if (null == bookID) {
-            req.setAttribute("errorHead", "Error! ");
-            req.setAttribute("error", "Book does not exist!");
+            Integer bookID = null;
+
+            try {
+                bookID = Integer.parseInt(req.getParameter("book-id"));
+            } catch (NumberFormatException e) {
+            }
+
+            if (null == bookID) {
+                req.setAttribute("errorHead", "Error! ");
+                req.setAttribute("error", "Book does not exist!");
+                req.getRequestDispatcher("browse").forward(req, resp);
+            }
+
+            Book book = BookService.getInstance().get(bookID);
+            BookService.getInstance().delete(book);
+            req.setAttribute("successHead", "Success! ");
+            req.setAttribute("success", "Book deleted from the library.");
             req.getRequestDispatcher("browse").forward(req, resp);
-        }
-
-        Book book = BookService.getInstance().get(bookID);
-        BookService.getInstance().delete(book);
-        req.setAttribute("successHead", "Success! ");
-        req.setAttribute("success", "Book deleted from the library.");
-        req.getRequestDispatcher("browse").forward(req, resp);
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (null == req.getParameter("book-id")){
+            req.setAttribute("errorHead", "You must pick a book to delete! ");
+            req.getRequestDispatcher("browse").forward(req, resp);
+        }
         req.getRequestDispatcher("book-delete.jsp").forward(req, resp);
     }
 }
