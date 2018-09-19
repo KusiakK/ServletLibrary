a8d34f18dbeac8309599ec476515e68c5a110ae4
KusiakK <kusiak.konrad@gmail.com>
2018-09-19 14:27:48 +0200

    ErrorMessenger used to handle errors in BookDeleteServlet

--- web/src/main/java/servlets/BookDeleteServlet.java
+++ web/src/main/java/servlets/BookDeleteServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import models.Book;
 import services.BookService;
+import utility.ServletStatics;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,43 +10,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/deleteBook")
 public class BookDeleteServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO use ErrorMessenger to handle errors
+        List<String> errorMessages = new ArrayList<>();
+
         if (!"confirm".equals(req.getParameter("deleteConfirmation"))) {
             resp.sendRedirect("browse");
             return;
         }
 
-            Integer bookID = null;
-
-            try {
-                bookID = Integer.parseInt(req.getParameter("book-id"));
-            } catch (NumberFormatException e) {
-            }
-
-            if (null == bookID) {
-                req.setAttribute("errorHead", "Error! ");
-                req.setAttribute("error", "Book does not exist!");
-                req.getRequestDispatcher("browse").forward(req, resp);
-            }
-
-            Book book = BookService.getInstance().get(bookID);
-            BookService.getInstance().delete(book);
-            req.setAttribute("successHead", "Success! ");
-            req.setAttribute("success", "Book deleted from the library.");
+        if (null == req.getParameter("book-id")) {
+            errorMessages.add("You must pick a book to delete!");
+            req.setAttribute(ServletStatics.ERROR_LIST_ATTRIBUTE, errorMessages);
             req.getRequestDispatcher("browse").forward(req, resp);
+        }
+
+        Integer bookID = null;
+
+        try {
+            bookID = Integer.parseInt(req.getParameter("book-id"));
+        } catch (NumberFormatException e) {
+            errorMessages.add("Book does not exist!");
+            req.setAttribute(ServletStatics.ERROR_LIST_ATTRIBUTE, errorMessages);
+            req.getRequestDispatcher("browse").forward(req, resp);
+        }
+
+        Book book = BookService.getInstance().get(bookID);
+        BookService.getInstance().delete(book);
+        req.setAttribute("success", "Book deleted from the library.");
+        req.getRequestDispatcher("browse").forward(req, resp);
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (null == req.getParameter("book-id")){
-            req.setAttribute("errorHead", "You must pick a book to delete! ");
+        if (null == req.getParameter("book-id")) {
+            req.setAttribute(ServletStatics.SINGLE_ERROR_ATTRIBUTE, "You must pick a book to delete! ");
             req.getRequestDispatcher("browse").forward(req, resp);
         }
         req.getRequestDispatcher("book-delete.jsp").forward(req, resp);
