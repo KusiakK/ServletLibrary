8f757642d4cfbd9ae592581ab939534df3aaff0f
KusiakK <kusiak.konrad@gmail.com>
2018-10-03 10:24:25 +0200

    login service created

--- service/src/main/java/services/UserService.java
+++ service/src/main/java/services/UserService.java
@@ -1,24 +1,37 @@
 package services;
 
+import exceptions.PasswordNotMatchingException;
 import models.User;
 import repositories.UserRepository;
+import exceptions.UserNotFoundException;
 
 public class UserService {
-    private UserService instance;
-    private UserRepository userRepository;
+    private static UserService instance;
+    private UserRepository repository;
 
     private UserService() {
-        userRepository = new UserRepository();
+        repository = new UserRepository();
     }
 
-    public UserService getInstance() {
+    public static UserService getInstance() {
         if (null == instance) {
             instance = new UserService();
         }
         return instance;
     }
 
-    public Boolean register(User user) {
-        return null != userRepository.create(user);
+    public User register(User user) {
+        return repository.create(user);
+    }
+
+    public User login(String userName, String password) throws UserNotFoundException, PasswordNotMatchingException {
+        User user = repository.findByUserName(userName);
+        if (null == user) {
+            throw new UserNotFoundException();
+        }
+        if (!user.getPassword().equals(password)) {
+            throw new PasswordNotMatchingException();
+        }
+        return user;
     }
 }
--- web/src/main/java/servlets/LoginServlet.java
+++ web/src/main/java/servlets/LoginServlet.java
@@ -1,18 +1,51 @@
 package servlets;
 
+import exceptions.PasswordNotMatchingException;
+import exceptions.UserNotFoundException;
+import models.User;
+import services.UserService;
+import utility.ServletUtility;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO handle login
+        List<String> errorMessages = new ArrayList<>();
+
+
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+
+
+        User user = null;
+        try {
+            user = UserService.getInstance().login(userName, password);
+        } catch (UserNotFoundException e) {
+            errorMessages.add("User name not found!");
+        } catch (PasswordNotMatchingException e) {
+            errorMessages.add("Wrong Password!");
+        } catch (Exception e) {
+            errorMessages.add("Unknown Error!");
+        }
+
+        if (null != user) {
+            req.getSession(true).setAttribute("userName", user.getUserName());
+            req.setAttribute("success", "You have been logged in!");
+            req.getRequestDispatcher("/home").forward(req, resp);
+        } else {
+            req.setAttribute(ServletUtility.ERROR_LIST_ATTRIBUTE, errorMessages);
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
     }
 
     @Override
