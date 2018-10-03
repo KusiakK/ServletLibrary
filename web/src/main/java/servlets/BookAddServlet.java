a22ad1986df2e0cdb4c82867557be6b2c59b50bb
blysku <blyskx@gmail.com>
2018-10-03 21:35:28 +0200

    BookAddServlet refactor

--- web/src/main/java/servlets/BookAddServlet.java
+++ web/src/main/java/servlets/BookAddServlet.java
@@ -25,51 +25,24 @@ public class BookAddServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<String> errorMessages = new ArrayList<>();
 
-        Book book = (Book) req.getAttribute("book");
-        if (null == book) {
-            book = new Book();
-        }
+        Author author = getAuthor(req);
+        LocalDate releaseDate = getLocalDate(req, errorMessages);
+        Integer pages = getPages(req, errorMessages);
 
-        Author author = null;
-
-        LocalDate releaseDate = null;
-
-        Integer pages = null;
-
-        try {
-            int authorID = Integer.parseInt(req.getParameter("author-id"));
-            author = AuthorService.getInstance().get(authorID);
-        } catch (Exception e) {
-        }
-
-        if (!req.getParameter("bookReleaseDate").isEmpty()) {
-            try {
-                String releaseDateString = req.getParameter("bookReleaseDate");
-                releaseDate = LocalDate.parse(releaseDateString);
-            } catch (NullPointerException e) {
-            } catch (DateTimeParseException e) {
-                errorMessages.add("Wrong date format! ");
-            }
-        }
-
-        if (!req.getParameter("bookPages").isEmpty()) {
-            try {
-                pages = Integer.parseInt(req.getParameter("bookPages"));
-            } catch (NumberFormatException e){
-                errorMessages.add("Wrong pages format!");
-            }
-        }
-
-        book.setIsbn(req.getParameter("isbn"));
-        book.setReleaseDate(releaseDate);
-        book.setTitle(req.getParameter("bookTitle"));
-        book.setCategory(req.getParameter("bookCategory"));
-        book.setPages(pages);
-        book.setAuthor(author);
-        book.setSummary(req.getParameter("bookSummary"));
+        Book book = createBook(req, author, releaseDate, pages);
 
         errorMessages.addAll(ErrorMessenger.getInstance().getMessages(book));
 
+        handleRedirection(resp, req, errorMessages, book);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("authors", AuthorService.getInstance().getAll());
+        req.getRequestDispatcher("book-add.jsp").forward(req, resp);
+    }
+
+    private void handleRedirection(HttpServletResponse resp, HttpServletRequest req, List<String> errorMessages, Book book) throws ServletException, IOException {
         if (!errorMessages.isEmpty()) {
             req.setAttribute("authors", AuthorService.getInstance().getAll());
             req.setAttribute("book", book);
@@ -89,9 +62,52 @@ public class BookAddServlet extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("authors", AuthorService.getInstance().getAll());
-        req.getRequestDispatcher("book-add.jsp").forward(req, resp);
+    private Book createBook(HttpServletRequest req, Author author, LocalDate releaseDate, Integer pages) {
+        Book book = (Book) req.getAttribute("book");
+        if (null == book) {
+            book = new Book();
+        }
+
+        book.setIsbn(req.getParameter("isbn"));
+        book.setReleaseDate(releaseDate);
+        book.setTitle(req.getParameter("bookTitle"));
+        book.setCategory(req.getParameter("bookCategory"));
+        book.setPages(pages);
+        book.setAuthor(author);
+        book.setSummary(req.getParameter("bookSummary"));
+
+        return book;
+    }
+
+    private Integer getPages(HttpServletRequest req, List<String> errorMessages) {
+        Integer pages = null;
+        if (!req.getParameter("bookPages").isEmpty()) {
+            try {
+                pages = Integer.parseInt(req.getParameter("bookPages"));
+            } catch (NumberFormatException e) {
+                errorMessages.add("Wrong pages format!");
+            }
+        }
+        return pages;
+    }
+
+    private Author getAuthor(HttpServletRequest req) {
+        Author author = null;
+        int authorID = Integer.parseInt(req.getParameter("author-id"));
+        author = AuthorService.getInstance().get(authorID);
+        return author;
+    }
+
+    private LocalDate getLocalDate(HttpServletRequest req, List<String> errorMessages) {
+        LocalDate releaseDate = null;
+        if (!req.getParameter("bookReleaseDate").isEmpty()) {
+            try {
+                String releaseDateParam = req.getParameter("bookReleaseDate");
+                releaseDate = LocalDate.parse(releaseDateParam);
+            } catch (DateTimeParseException e) {
+                errorMessages.add("Wrong date format! ");
+            }
+        }
+        return releaseDate;
     }
 }
--- web/src/test/java/servlets/LoginServletTest.java
+++ web/src/test/java/servlets/LoginServletTest.java
@@ -1,8 +1,6 @@
 package servlets;
 
 import org.junit.Test;
-import org.mockito.Mockito;
-import utility.ServletUtility;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +9,6 @@ import javax.servlet.http.HttpSession;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -24,35 +21,51 @@ public class LoginServletTest {
 
     @Test
     public void testLoginServletDoPostForSuccessfulLogin() throws Exception {
-
-        String login = "correctLogin";
-        String password = "correctPassword";
-
-        when(req.getParameter("login")).thenReturn(login);
-        when(req.getParameter("password")).thenReturn(password);
-
+        String correctLogin = "correctLogin";
+        String correctPassword = "correctPassword";
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
+        when(req.getParameter("login")).thenReturn(correctLogin);
+        when(req.getParameter("password")).thenReturn(correctPassword);
         when(resp.getWriter()).thenReturn(writer);
-
-
         when(req.getSession(true)).thenReturn(session);
-        doNothing().when(session).setAttribute("login", login);
-
+        doNothing().when(session).setAttribute("login", correctLogin);
         doNothing().when(req).setAttribute("success", "You have been logged in!");
-
         when(req.getRequestDispatcher("/home")).thenReturn(dispatcher);
         doNothing().when(dispatcher).forward(req, resp);
-
 
         new LoginServlet().doPost(req, resp);
 
         verify(req, atLeastOnce()).getParameter("login");
         verify(req, atLeastOnce()).getParameter("password");
         verify(req, atLeastOnce()).getSession(true);
-        verify(session, atLeastOnce()).setAttribute("login", login);
+        verify(session, atLeastOnce()).setAttribute("login", correctLogin);
         verify(req, atLeastOnce()).getRequestDispatcher("/home");
         verify(dispatcher, atLeastOnce()).forward(req, resp);
+    }
 
+    @Test
+    public void testIfCannotLoginWithIncorrectData() throws Exception {
+        String incorrectLogin = "incorrect login";
+        String incorrectPassword = "incorrect password";
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(req.getParameter("login")).thenReturn(incorrectLogin);
+        when(req.getParameter("password")).thenReturn(incorrectPassword);
+        when(resp.getWriter()).thenReturn(writer);
+        when(req.getSession(true)).thenReturn(session);
+        doNothing().when(session).setAttribute("login", incorrectLogin);
+        doNothing().when(req).setAttribute("success", "You have been logged in!");
+        when(req.getRequestDispatcher("login.jsp")).thenReturn(dispatcher);
+        doNothing().when(dispatcher).forward(req, resp);
+
+        new LoginServlet().doPost(req, resp);
+
+        verify(req, atLeastOnce()).getParameter("login");
+        verify(req, atLeastOnce()).getParameter("password");
+        verify(req, never()).getSession(true);
+        verify(session, never()).setAttribute("login", incorrectLogin);
+        verify(req, atLeastOnce()).getRequestDispatcher("login.jsp");
+        verify(dispatcher, atLeastOnce()).forward(req, resp);
     }
 }
