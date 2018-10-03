500cdb51eb55cc4dd0aa8b35757c597f806e1db2
KusiakK <kusiak.konrad@gmail.com>
2018-10-03 14:33:58 +0200

    integration test for login finished; fixed bug in maven dependencies

--- web/src/test/java/servlets/LoginServletTest.java
+++ web/src/test/java/servlets/LoginServletTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -18,11 +19,12 @@ public class LoginServletTest {
     private HttpServletResponse resp = mock(HttpServletResponse.class);
 
     @Test
-    public void testLoginServletDoPostForSuccessLogin() throws Exception {
+    public void testLoginServletDoPostForSuccessfulLogin() throws Exception {
         RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        HttpSession session = mock(HttpSession.class);
 
         User user = new User();
-        user.setLogin("correctName");
+        user.setLogin("correctLogin");
         user.setPassword("correctPassword");
 
         when(req.getParameter("login")).thenReturn(user.getLogin());
@@ -32,16 +34,22 @@ public class LoginServletTest {
         PrintWriter writer = new PrintWriter(stringWriter);
         when(resp.getWriter()).thenReturn(writer);
 
+
+        when(req.getSession(true)).thenReturn(session);
+        doNothing().when(session).setAttribute("login", user.getLogin());
+
+        doNothing().when(req).setAttribute("success", "You have been logged in!");
+
         when(req.getRequestDispatcher("/home")).thenReturn(dispatcher);
         doNothing().when(dispatcher).forward(req, resp);
 
         new LoginServlet().doPost(req, resp);
 
-        verify(req, atLeast(1)).getParameter(user.getLogin());
-        verify(req, atLeast(1)).getParameter(user.getPassword());
-
-        writer.flush();
-
-        System.out.println(stringWriter);
+        verify(req, atLeastOnce()).getParameter("login");
+        verify(req, atLeastOnce()).getParameter("password");
+        verify(req, atLeastOnce()).getSession(true);
+        verify(session, atLeastOnce()).setAttribute("login", user.getLogin());
+        verify(req, atLeastOnce()).getRequestDispatcher("/home");
+        verify(dispatcher, atLeastOnce()).forward(req, resp);
     }
 }
